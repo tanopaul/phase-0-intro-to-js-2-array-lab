9321db2dece942abeb3532e58116ae2ecad6eb70
Tano Paul <tanopaul38@gmail.com>
2023-04-19 13:43:58 -0400

    completed arrays prework

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+   const newCats = cats.slice();
+   newCats.push('Broom');
+   return newCats;
+}
+
+function prependCat(name) {
+    const newerCats = cats.slice();
+    newerCats.unshift('Arnold');
+    return newerCats;
+}
+
+function removeLastCat() {
+    const lastCatRemoved = cats.slice();
+    lastCatRemoved.pop();
+    return lastCatRemoved
+}
+
+function removeFirstCat() {
+    const firstCatRemoved = cats.slice();
+    firstCatRemoved.shift();
+    return firstCatRemoved;
+}
